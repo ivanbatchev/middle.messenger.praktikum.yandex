50476b75d1afff8feb73c085e5ab47265c73c3fc
Ivan Batchev <batchev.ivan@yandex.ru>
2024-02-11 23:55:55 +0300

    templates update

--- src/pages/error/index.js
+++ src/pages/error/index.js
@@ -1,6 +1,6 @@
 import Handlebars from 'handlebars';
-import tmpl from './not-found.hbs?raw';
-import './not-found.scss';
+import tmpl from './error.hbs?raw';
+import './error.scss';
 
 export default (props = {}) => {
 	return Handlebars.compile(tmpl)(props);
